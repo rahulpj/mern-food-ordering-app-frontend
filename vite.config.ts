a7b77f0c604659176072419cc6c6ad2d92377042
Rahul Panjwani <rahulpanjwani2003@gmail.com>
2024-10-06 03:47:09 +0530

    removed unnecessary lines

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,10 @@
+import { fileURLToPath } from "url";
 import path from "path";
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
+
+// Use `import.meta.url` to get the current file path and create an equivalent `__dirname`
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
 export default defineConfig({
   plugins: [react()],
